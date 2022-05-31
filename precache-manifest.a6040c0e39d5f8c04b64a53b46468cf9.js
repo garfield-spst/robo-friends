428422071db5b4d6baa2454ebfe711aa37116998
Surya Pratap <garfieldthesurya01@gmail.com>
2022-05-31 15:31:16 +0530

    Updates

---
+++ precache-manifest.a6040c0e39d5f8c04b64a53b46468cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad34416054366d9e6c9",
+    "url": "/robo-friends/static/css/main.0acfe75e.chunk.css"
+  },
+  {
+    "revision": "1ad34416054366d9e6c9",
+    "url": "/robo-friends/static/js/main.1ad34416.chunk.js"
+  },
+  {
+    "revision": "13c66b73d8e9449724ab",
+    "url": "/robo-friends/static/css/1.c2a6189b.chunk.css"
+  },
+  {
+    "revision": "13c66b73d8e9449724ab",
+    "url": "/robo-friends/static/js/1.13c66b73.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "58acaaeafe24c03b2eddd6c3599873ff",
+    "url": "/robo-friends/index.html"
+  }
+];
