bfb48fb5f505f4a7f7edcac537ff143dab328e36
Surya Pratap <garfieldthesurya01@gmail.com>
2022-05-31 15:22:01 +0530

    added link of source code and personal website

---
+++ src/containers/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+        <div className="tc grow br3 pa3 ma2 dib bw2">
+          <a
+            className="link"
+            href="https://github.com/garfield-spst/robo-friends"
+            target="_blank"
+          >
+            Source Code
+          </a>
+        </div>
+        <div className="tc grow br3 pa3 ma2 dib bw2">
+          <a className="link" href="http://garfield-spst.xyz" target="_blank">
+            Surya Pratap Singh
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
